d9de1b5fd13af308ff160ed21ebd436da3f0e1ee
eslamhamed39 <eslamhamed39@gmail.com>
2025-07-13 11:40:32 +0300

    Update base href in index.html for correct relative paths and enable hash-based routing in app-routing.module.ts for improved navigation handling.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,11 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
